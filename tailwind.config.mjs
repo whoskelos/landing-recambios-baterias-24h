037dee52c6589be19504578f9ed04db7c5841de3
Manuel Retamozo <michael.retamozo@gmail.com>
2024-04-22 11:46:37 +0200

    add(navegación): se agrega barra navegación responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,15 @@
+import animations from "@midudev/tailwind-animations";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			height: {
-				'hero': 'calc(100svh - 3.5rem)'
-			}
-		},
-	},
-	plugins: [],
-	darkMode: 'selector'
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      height: {
+        hero: "calc(100svh - 3.5rem)",
+      },
+    },
+  },
+  plugins: [animations],
+  darkMode: "selector",
+};

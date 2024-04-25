c617155e67ebf497b2c701c32246cdc8718fc8f2
Manuel Retamozo <michael.retamozo@gmail.com>
2024-04-26 00:29:18 +0200

    algunos cambios xd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,16 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        "fade-scroll": "fade-scroll linear both"
+      },
+      keyframes:{
+        "fade-scroll": {
+          to: {
+            opacity: 1,
+          }
+        }
+      },
       height: {
         hero: "calc(100svh - 3.5rem)",
       },
